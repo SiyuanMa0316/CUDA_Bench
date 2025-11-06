6270f127ee4ed5a14f4c935e1591c776a5b3e11c
SiyuanMa0316 <masiyuan0@gmail.com>
2025-11-06 09:02:34 -0600

    fix run_all.sh

--- data_and_scripts/run_all.sh
+++ data_and_scripts/run_all.sh
@@ -8,20 +8,19 @@ num_tests=${#dim_i[@]}
 mkdir -p logs
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "../src/gemm/gemm_naive_float -wA=${dim_k[i]} -hA=${dim_i[i]} -wB=${dim_j[i]} -hB=${dim_k[i]}" -o logs/gemm_cuda_fp32_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "../src/gemm/gemm_naive_float -wA=${dim_k[i]} -hA=${dim_i[i]} -wB=${dim_j[i]} -hB=${dim_k[i]}" -o logs/gemm_cuda_fp32_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "python3 ../src/gemm/gemm_torch.py -i 1000 -p fp32 --gpu ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_torch_fp32_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "python3 ../src/gemm/gemm_torch.py -i 1000 -p fp32 --gpu ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_torch_fp32_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "../bin/gemm_cuda_bench --usecublas --cudacoresonly --mulprecision fp32 --accprecision fp32 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cublas_fp32_cc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "../bin/gemm_cuda_bench --usecublas --cudacoresonly --mulprecision fp32 --accprecision fp32 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cublas_fp32_cc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "../bin/gemm_cuda_bench --cudacoresonly --mulprecision fp32 --accprecision fp32 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cutlass_fp32_cc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
-
+  ../nvml_gpu_profiler -c "../bin/gemm_cuda_bench --cudacoresonly --mulprecision fp32 --accprecision fp32 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cutlass_fp32_cc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
@@ -29,19 +28,19 @@ for ((i=0; i<num_tests; i++)); do
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "../bin/gemm_cuda_bench --cudacoresonly --mulprecision fp16 --accprecision fp16 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cutlass_fp16_cc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "../bin/gemm_cuda_bench --cudacoresonly --mulprecision fp16 --accprecision fp16 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cutlass_fp16_cc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "python3 ../src/gemm/gemm_torch.py -i 1000 -p fp16 --gpu ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_torch_fp16_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "python3 ../src/gemm/gemm_torch.py -i 1000 -p fp16 --gpu ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_torch_fp16_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "../bin/gemm_cuda_bench --usecublas --mulprecision fp16 --accprecision fp16 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cublas_fp16_tc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "../bin/gemm_cuda_bench --usecublas --mulprecision fp16 --accprecision fp16 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cublas_fp16_tc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 for ((i=0; i<num_tests; i++)); do
-  ./nvml_gpu_profiler -c "../bin/gemm_cuda_bench --mulprecision fp16 --accprecision fp16 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cutlass_fp16_tc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
+  ../nvml_gpu_profiler -c "../bin/gemm_cuda_bench --mulprecision fp16 --accprecision fp16 --iterations 1000 ${dim_i[i]} ${dim_j[i]} ${dim_k[i]}" -o logs/gemm_cutlass_fp16_tc_${dim_i[i]}x${dim_j[i]}x${dim_k[i]}_nvml_log.csv
 done
 
 
